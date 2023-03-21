e2196b569320e57a1def8095555cdcf29f43de4f
Huichen Ma <mafengming2020@gmail.com>
2023-03-21 11:15:46 -0700

    change program builder

--- src/main/java/cse232b/visitor/XqueryProgramBuilder.java
+++ src/main/java/cse232b/visitor/XqueryProgramBuilder.java
@@ -22,12 +22,13 @@ import org.antlr.v4.runtime.tree.ParseTree;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 
+import cse232b.parsers.XQueryBaseVisitor;
 import cse232b.parsers.XQueryLexer;
 import cse232b.parsers.XQueryParser;
 
 public class XqueryProgramBuilder {
 	// parse input XPath query, build tree, retrieve results
-	private static ArrayList<Node> retrieveXQueryResult(String inputFile) {
+	private static ArrayList<Node> retrieveXQueryResult(String inputFile, String pattern) {
 		// build antlr input file stream
 		FileInputStream fileInputStream = null;
 		ANTLRInputStream antlrInputStream = null;
@@ -38,16 +39,28 @@ public class XqueryProgramBuilder {
 			System.out.println("Input file error!");
 			System.exit(1);
 		}
+		// retrieve result
+		// base pattern
+		if (pattern.equals("0")) {
+			XqueryExpressionBuilder builder = new XqueryExpressionBuilder();
+			return retrieveExpressionBuilderResult(antlrInputStream, builder);
+		}
+		// optimized pattern -- need implement here
+
+		return result;
+	}
+
+	// retrieve results from different expression builder
+	private static <T> T retrieveExpressionBuilderResult(ANTLRInputStream antlrInputStream,
+			XQueryBaseVisitor<T> builder) {
 		// build Antlr parse tree
 		XQueryLexer lexer = new XQueryLexer(antlrInputStream);
 		CommonTokenStream tokenStream = new CommonTokenStream(lexer);
 		XQueryParser xQueryParser = new XQueryParser(tokenStream);
 		// change from rp to xq
 		ParseTree tree = xQueryParser.xq();
-		XqueryExpressionBuilder builder = new XqueryExpressionBuilder();
-		// retrieve results
-		ArrayList<Node> result = builder.visit(tree);
-		return result;
+
+		return builder.visit(tree);
 	}
 
 	// generate XML document
@@ -62,6 +75,7 @@ public class XqueryProgramBuilder {
 			System.exit(1);
 		}
 		Document document = documentBuilder.newDocument();
+		document.setXmlStandalone(true);
 		// Add query result to xml object
 		// single node should not surround by "RESULT"
 		if (result.size() == 1) {
@@ -92,6 +106,7 @@ public class XqueryProgramBuilder {
 			System.exit(1);
 		}
 		transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+		transformer.setOutputProperty(OutputKeys.STANDALONE, "yes");
 		transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
 		return transformer;
 	}
@@ -99,15 +114,22 @@ public class XqueryProgramBuilder {
 	public static void main(String[] args) {
 		// build engine
 		// System.out.println("test");
-		// input arguments' length should be 2
-		if (args.length != 2) {
-			System.out.println("Input argument number should be 2!");
+		// input arguments' length should be 3
+		if (args.length != 2 && args.length != 3) {
+			System.out.println("Input argument number should be 3!");
+			System.out.println("Usage: java -jar cse232b_milestone3.jar [input file] [output file] [0,1]");
+			System.out.println("0 represents basic join pattern; 1 represents optimized pattern; 0 is default.");
 			System.exit(1);
 		}
+		// get input file; output file; pattern
 		String inputFile = args[0];
 		String outputFile = args[1];
+		String pattern = "0";
+		if (args.length == 3) {
+			pattern = args[2];
+		}
 		// Parse Input Query, retrieve results
-		ArrayList<Node> result = retrieveXQueryResult(inputFile);
+		ArrayList<Node> result = retrieveXQueryResult(inputFile, pattern);
 		// System.out.println(result);
 		// Generate output xml object
 		Document document = xmlDocumentGenerator(result);
